3bb12fa41c90722f1e5f73c14a91d70b6e246857
nafiz Alam <nafizalam.dev@gmail.com>
2026-05-13 00:41:01 +0600

    Create update user route

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -21,6 +21,22 @@ export const createUser = async (formData) => {
     return data;
 }
 
+export const updateUser = async (formData) => {
+    'use server';
+    
+    const updatedUser = Object.fromEntries(formData.entries());
+    const res = await fetch(``, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedUser)
+    })
+    const data = await res.json();
+
+    return data;
+}
+
 export const deleteUser = async (userId) => {
     'use server';
 
